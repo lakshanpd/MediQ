9f60141ee4fc634f79b0a26479a9b4d1cf4455a3
lakshanpd <lakshanpd.cse21@gmail.com>
2025-10-05 23:55:57 +0530

    feat: add expo notification handler

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,8 +1,22 @@
 import { View, Text, Button } from "react-native";
 import { useRouter } from "expo-router";
+import * as Notifications from "expo-notifications";
+import { useEffect } from "react";
 
 export default function WelcomeScreen() {
   const router = useRouter();
+
+  useEffect(() => {
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: false,
+        shouldShowBanner: true,
+        shouldShowList: true,
+      }),
+    });
+  }, []);
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
