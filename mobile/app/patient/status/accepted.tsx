6f6649fca2a51d98697ec422ba3c464e9607eff0
lakshanpd <lakshanpd.cse21@gmail.com>
2025-10-07 21:17:42 +0530

    feat: create sample accepted and rejected pages

--- mobile/app/patient/status/accepted.tsx
+++ mobile/app/patient/status/accepted.tsx
@@ -1,9 +1,34 @@
-import { Text, View } from "react-native";
+import { useUser } from "@/contexts/userContext";
+import React from "react";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
 
 export default function PatientAcceptedScreen() {
-    return (
-        <View>
-            <Text>Patient Accepted Screen</Text>
-        </View>
-    );
+    const { resetUser } = useUser();
+  const handleRejected = async () => {
+    await resetUser()
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Your appointment was accepted</Text>
+
+      <TouchableOpacity style={styles.rejectedButton} onPress={handleRejected}>
+        <Text style={styles.rejectedButtonText}>Reset All</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 20, justifyContent: "center", backgroundColor: "#fff" },
+  title: { fontSize: 18, fontWeight: "600", marginBottom: 20, textAlign: "center" },
+  rejectedButton: {
+    backgroundColor: "#ff3b30",
+    paddingVertical: 12,
+    borderRadius: 8,
+    alignItems: "center",
+    alignSelf: "center",
+    paddingHorizontal: 24,
+  },
+  rejectedButtonText: { color: "#fff", fontSize: 16, fontWeight: "600" },
+});
--- mobile/app/patient/status/rejected.tsx
+++ mobile/app/patient/status/rejected.tsx
@@ -1,9 +1,44 @@
-import { Text, View } from "react-native";
+import { useUser } from "@/contexts/userContext";
+import React from "react";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
 
 export default function PatientRejectedScreen() {
+  const { resetUser } = useUser();
+  const handleRejected = async () => {
+    await resetUser();
+  };
+
   return (
-    <View>
-      <Text>Patient Rejected Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Your appointment was rejected</Text>
+
+      <TouchableOpacity style={styles.rejectedButton} onPress={handleRejected}>
+        <Text style={styles.rejectedButtonText}>Reset All</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginBottom: 20,
+    textAlign: "center",
+  },
+  rejectedButton: {
+    backgroundColor: "#ff3b30",
+    paddingVertical: 12,
+    borderRadius: 8,
+    alignItems: "center",
+    alignSelf: "center",
+    paddingHorizontal: 24,
+  },
+  rejectedButtonText: { color: "#fff", fontSize: 16, fontWeight: "600" },
+});
