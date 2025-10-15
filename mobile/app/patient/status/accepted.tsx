94f719a3e19bb1c20f6f06bae76a8d74bb39e993
NMWrobotics <nimethm5@gmail.com>
2025-10-15 21:30:05 +0530

    UI Updates - Implement token accepted screen

--- mobile/app/patient/status/accepted.tsx
+++ mobile/app/patient/status/accepted.tsx
@@ -1,21 +1,133 @@
 import { useUser } from "@/contexts/userContext";
 import React from "react";
-import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, StatusBar, Image, Pressable } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { MediQImages } from "@/constants/theme";
+import { db } from "@/firebaseConfig";
+import { useDoctorListener } from "@/hooks/useDoctorListener";
+import { useSessionListener } from "@/hooks/useSessionListener";
+import { useTokenListener } from "@/hooks/useTokenListener";
+import { doc, serverTimestamp, updateDoc } from "firebase/firestore";
 
 export default function PatientAcceptedScreen() {
+
+    const { userState, setPatientStatus } = useUser();
+    const tokenData = useTokenListener(userState?.userData?.tokenId ?? null);
+    const { sessionData } = useSessionListener(tokenData?.session_id ?? null);
+    const { doctorData } = useDoctorListener(sessionData?.doctor_id ?? null);
     const { resetUser } = useUser();
   const handleRejected = async () => {
     await resetUser()
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Your appointment was accepted</Text>
+<View className="flex-1 bg-white"> 
+            <StatusBar barStyle="dark-content" />
+      
+            <Image
+              source={MediQImages.main_bg_top}
+              className="absolute inset-0 w-full h-full"
+              resizeMode="cover"
+              accessible={false}
+            />
+            <SafeAreaView className="flex-1">
+          <View className="flex items-center mb-4 ">
+            <Image
+              source={MediQImages.mediq_inline_logo}
+              className="w-48 h-20  flex mt-10 mb-10"
+              resizeMode="contain"
+            />
+              <Text className="text-2xl font-bold text-mediq-blue text-center mx-8 ">
+              Your token has been Accepted
+            </Text>
 
-      <TouchableOpacity style={styles.rejectedButton} onPress={handleRejected}>
-        <Text style={styles.rejectedButtonText}>Reset All</Text>
-      </TouchableOpacity>
-    </View>
+          </View>
+          <View className="bg-mediq-lightest-grey rounded-2xl p-6 mx-6 mt-2 mb-6 flex-1">
+            <Text className="text-2xl font-bold text-mediq-blue ">
+              Dr. {doctorData?.first_name || 'Loading...'} {doctorData?.last_name || ''}
+            </Text>
+            <Text className="text-lg font-normal text-mediq-text-black mb-2">
+               {doctorData?.specialization || 'Loading...'}
+            </Text>
+          <View className = "flex-row justify-between mt-3">
+            <Text className="text-xl text-mediq-text-black font-semibold">
+              {sessionData?.start_time ? new Date(sessionData.start_time).toLocaleDateString() : 'Loading...'}
+            </Text>
+            <Text className="text-xl text-mediq-text-black font-semibold">
+              {sessionData?.start_time && sessionData?.end_time 
+                  ? `${new Date(sessionData.start_time).toLocaleTimeString()} - ${new Date(sessionData.end_time).toLocaleTimeString()}`
+                  : 'Loading...'
+              }
+            </Text>
+            </View>
+            <View className="flex-row justify-end mt-2">
+              <Text className="text-lg font-medium text-mediq-light-blue mb-2">
+                Medihelp, Ratmalana
+              </Text>
+            </View>
+          <View className="border-b border-mediq-blue mb-2" />
+
+          <View className="flex mt-2 mb-3">
+            <Text className="text-md font-bold text-mediq-blue">
+              Name
+            </Text>
+            <Text className="text-lg text-mediq-text-black font-medium pl-2">
+               {tokenData?.patient?.name || 'N/A'}
+            </Text>
+          </View>
+          <View className="flex mb-3">
+            <Text className="text-md font-bold text-mediq-blue">
+              Birthday
+            </Text>
+            <Text className="text-lg text-mediq-text-black font-medium pl-2">
+              {tokenData?.patient?.birthday || 'N/A'}
+            </Text>
+          </View>
+          <View className="flex mb-3">
+            <Text className="text-md font-bold text-mediq-blue">
+              Contact
+            </Text>
+            <Text className="text-lg text-mediq-text-black font-medium pl-2">
+              {tokenData?.patient?.contact_number || 'N/A'}
+            </Text>
+          </View>
+          <View className="border-b border-mediq-blue mb-2" />
+
+          <View className="flex items-center justify-center mt-2">
+            <Text className="text-md font-bold text-mediq-blue">
+              Your Token Number
+            </Text>
+            <Text className="text-8xl text-mediq-blue font-semibold mt-3">
+              28
+            </Text>
+            </View>
+
+            <View className="flex items-end justify-center -mt-10">
+            <Text className="text-sm font-bold text-mediq-blue">
+              Expected Time
+            </Text>
+            <Text className="text-lg text-mediq-text-black font-medium">
+              9.30 PM
+            </Text>
+            </View>
+
+          </View>
+
+
+
+          <View className="h-16 px-6 mb-6  ">
+            <Pressable
+              onPress={handleRejected}
+              className="h-16 rounded-2xl bg-mediq-red flex-row items-center justify-center active:scale-95">
+              <Text className="text-xl text-white font-bold">
+                Cancel
+              </Text>
+            
+                    </Pressable>
+                  </View>
+          
+          </SafeAreaView>
+            </View>
   );
 }
 
