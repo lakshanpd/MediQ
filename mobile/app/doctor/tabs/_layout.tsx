0bc0d133dd3dfe6e166414836c1021bf7de642e9
NMWrobotics <nimethm5@gmail.com>
2025-10-19 19:45:17 +0530

    UI Updates - Doctor Navbar created

--- mobile/app/doctor/tabs/_layout.tsx
+++ mobile/app/doctor/tabs/_layout.tsx
@@ -1,19 +1,66 @@
-import React from "react";
+import { MediQImages } from "@/constants/theme";
 import { Tabs } from "expo-router";
-import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import { Image } from "react-native";
 
 export default function DoctorLayout() {
   return (
     <Tabs
       screenOptions={({ route }) => ({
         headerShown: false,
-        tabBarActiveTintColor: "#007AFF",
-        tabBarInactiveTintColor: "#8e8e93",
+        tabBarActiveTintColor: "#0564F5",
+        tabBarInactiveTintColor: "#404040",
+        tabBarStyle: {
+          backgroundColor: "#FFFFFF",
+          borderTopWidth: 1,
+          borderTopColor: "#E5E7EB",
+          height: 90,
+          paddingBottom: 25,
+          paddingTop: 10,
+        },
+        tabBarLabelStyle: {
+          fontSize: 14,
+          fontWeight: "600",
+          marginTop: 5,
+        }
       })}
     >
-      <Tabs.Screen name="queue" options={{ title: "Queue" }} />
-      <Tabs.Screen name="requests" options={{ title: "Requests" }} />
-      <Tabs.Screen name="settings" options={{ title: "Settings" }} />
+      <Tabs.Screen
+        name="queue"
+        options={{
+          title: "Sessions",
+          tabBarIcon: ({ focused, color }) => (
+            <Image
+              source={MediQImages.navbar_icon_sessions}
+              style={{ tintColor: color, width: 28, height: 28 }}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="requests"
+        options={{
+          title: "Requests",
+          tabBarIcon: ({ focused, color }) => (
+            <Image
+              source={MediQImages.navbar_icon_requests}
+              style={{ tintColor: color, width: 28, height: 28}}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ focused, color }) => (
+            <Image
+              source={MediQImages.navbar_icon_settings}
+              style={{ tintColor: color, width: 28, height: 28 }}
+            />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
