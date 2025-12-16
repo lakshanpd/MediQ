fd6e1494a3ad91ae318b581f48f8390a04d61284
lakshanpd <lakshanpd.cse21@gmail.com>
2025-12-17 01:31:07 +0530

    add function to notify doctors when a request comes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,14 @@
 import {setGlobalOptions} from "firebase-functions";
-import {onDocumentUpdated} from "firebase-functions/v2/firestore";
+import {onDocumentCreated, onDocumentUpdated} from "firebase-functions/v2/firestore";
 import * as admin from "firebase-admin";
 import {sendPush} from "./push";
 
 setGlobalOptions({maxInstances: 1});
 
 admin.initializeApp();
+const db = admin.firestore();
 
-// notify user when token is accepted,rejected and started to serve
+// notify patient when token is accepted,rejected and started to serve
 export const notifyOnTokenStatusChange = onDocumentUpdated(
   {
     document: "tokens/{tokenId}",
@@ -24,7 +25,7 @@ export const notifyOnTokenStatusChange = onDocumentUpdated(
     const pushToken = after.device_token;
     if (!pushToken) return;
 
-    if (before.status === "pending" && after.status === "accepted") {
+    if (after.status === "accepted") {
       await sendPush(
         pushToken,
         "Token Accepted",
@@ -35,7 +36,7 @@ export const notifyOnTokenStatusChange = onDocumentUpdated(
         }
       );
     }
-    else if (before.status === "pending" && after.status === "rejected") {
+    else if (after.status === "rejected") {
       await sendPush(
         pushToken,
         "Token Rejected",
@@ -59,3 +60,56 @@ export const notifyOnTokenStatusChange = onDocumentUpdated(
     }
   }
 );
+
+// notify doctor when a request comes
+export const notifyDoctorOnNewRequest = onDocumentCreated(
+  {
+    document: "tokens/{tokenId}",
+    region: "asia-south1",
+  },
+  async (event) => {
+    const tokenData = event.data?.data();
+    if (!tokenData) return;
+
+    const sessionId = tokenData.session_id;
+    if (!sessionId) return;
+
+    // get session
+    const sessionSnap = await db.collection("sessions").doc(sessionId).get();
+    if (!sessionSnap.exists) return;
+
+    const sessionData = sessionSnap.data();
+    const doctorId = sessionData?.doctor_id;
+    if (!doctorId) return;
+
+    // find doctor by uid (NOT document ID)
+    const doctorQuery = await db
+      .collection("doctors")
+      .where("uid", "==", doctorId)
+      .limit(1)
+      .get();
+
+    if (doctorQuery.empty) return;
+    
+
+    const doctorDoc = doctorQuery.docs[0];
+    const doctorData = doctorDoc.data();
+
+    const deviceToken = doctorData?.device_token;
+    if (!deviceToken) return;
+
+    // send notification
+    await sendPush(
+      deviceToken,
+      "New Appointment Request 📩",
+      "A new patient request has been received. Please review it.",
+      {
+        tokenId: event.params.tokenId,
+        sessionId,
+        type: "NEW_REQUEST",
+      }
+    );
+  }
+);
+
+// notify doctor when session starts in 30 minutes and end in 10 minutes ---> onSchedule (check every minute)
